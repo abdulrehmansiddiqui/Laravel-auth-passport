77d407a437812741af5ae9e9aa7cbb74dd9991a9
Abdul Rehman <arehmans@live.com>
2020-10-04 05:15:59 -0700

    login register middleware

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'Usercontroller@register');
+Route::post('login', 'Usercontroller@login');
+
+Route::group(['middleware' => 'auth:api'],  function () {
+    Route::post('details', 'Usercontroller@details');
+});
